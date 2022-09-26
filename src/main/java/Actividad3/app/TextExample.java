f10bfde18973c8c925349bd0406c4f6a7188ce97
Ivan <ivanfergarcia@gmail.com>
2022-09-26 14:14:49 +0200

    Actividad 3

--- src/main/java/Actividad3/app/TextExample.java
+++ src/main/java/Actividad3/app/TextExample.java
@@ -9,12 +9,9 @@ public class TextExample {
 
     public static void main(String[] args) {
         TextService textService = new TextService();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce el nombre del archivo a crear: ");
-        String name = scanner.nextLine();
 
-        System.out.println("Introduce el texto a guardar en el archivo: ");
-        String text = scanner.nextLine();
+        String name = textService.requestName();
+        String text = textService.requestText();
 
         File file = textService.createFile(name, text);
 
--- src/main/java/Actividad3/service/TextService.java
+++ src/main/java/Actividad3/service/TextService.java
@@ -4,10 +4,23 @@ import main.java.Actividad3.dao.TextDAO;
 import main.java.Actividad3.dao.TextDAOImpl;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class TextService {
 
     private final TextDAO textDAO = new TextDAOImpl();
+
+    public String requestName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el nombre del archivo a crear: ");
+        return scanner.nextLine();
+    }
+
+    public String requestText() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el texto a guardar en el archivo: ");
+        return scanner.nextLine();
+    }
 
     public File createFile(String name, String text) {
         File file = textDAO.createFile(name, text);
